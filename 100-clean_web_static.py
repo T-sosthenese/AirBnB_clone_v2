14bb5143b6a2ffd7799d0b323b9f16cac18635e4
sosthene <sosthenetimi94@gmail.com>
2023-09-07 14:55:22 +0300

    cleaning archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -73,12 +73,12 @@ def do_clean(number=0):
         archives = archives[start:]
     else:
         archives = []
-        for archive in archives:
-            os.unlink('versions/{}'.format(archive))
-            cmd_parts = [
-                "rm -rf $(",
-                "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
-                " '/data/web_static/releases/web_static_.*'",
-                " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
-            ]
-            run(''.join(cmd_parts))
+    for archive in archives:
+        os.unlink('versions/{}'.format(archive))
+    cmd_parts = [
+        "rm -rf $(",
+        "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
+        " '/data/web_static/releases/web_static_.*'",
+        " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
+    ]
+    run(''.join(cmd_parts))
