b32b347774904f9ef9eab03b40fbc1bc0876b950
sosthene <sosthenetimi94@gmail.com>
2023-09-24 17:18:17 +0300

    Flask app

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""""
+Importing Flask to run our hbnb web app.
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """
+    Renders states_list htmf to display states created.
+    """
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ Removes current sqlalchemy session."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
